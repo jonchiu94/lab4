70407b262b287589adf1785e625eb9f5d7441441
jonchiu94 <jchiu84@my.bcit.ca>
2019-01-29 10:41:28 -0800

    adding codeigniter
    Signed-off-by: jonchiu94 <jchiu84@my.bcit.ca>

---
+++ app/Controllers/Home.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Home extends Controller
+{
+	public function index()
+	{
+		return view('welcome_message');
+	}
+
+	public function jono(){
+        return view('jono');
+    }
+
+}
