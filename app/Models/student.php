83f862031a38504d989a35472fe6edd64139479d
JacobSmith <jacob13smith@gmail.com>
2019-01-29 11:32:32 -0800

    added code to student model

--- app/Models/student.php
+++ app/Models/student.php
@@ -1,6 +1,21 @@
 use CodeIgniter\Model;
 
-class student extends Model
-{
+class student extends Model {
+	protected $data = [
+		'jacob' => ['id' => 'A01005350', 'name' => 'Jacob Smith', 'gpa' => 98],
+		'harman' => ['id' => 'A00994245', 'name' => 'Harmanbir Dhillon', 'gpa' => 103]
+		'jono' => ['id' => 'A01027608', 'name' => 'Jonthan Chiu', 'gpa' => 53]
+	];
 
+	public function find($id){
+		foreach ($data as $entry){
+			if ($entry[0]["id"] == $id){
+				return $entry[0];
+			}
+		}
+	}
+
+	public function findAll(){
+		return $data;
+	}
 }
